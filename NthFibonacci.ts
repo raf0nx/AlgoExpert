72b0d1b0d8f5eaee9d9d6181581bbd21d5907457
raf0nx <rafalpawlowski2011@gmail.com>
2022-08-06 21:53:26 +0200

    Add second (iterative) solution to Nth Fibonacci algorithm

--- NthFibonacci.ts
+++ NthFibonacci.ts
@@ -1,3 +1,14 @@
 // Solution 1, O(2^n) time complexity, O(n) space complexity
 export const getNthFib = (n: number): number =>
   n === 1 ? 0 : n <= 3 ? 1 : getNthFib(n - 1) + getNthFib(n - 2)
+
+// Solution 2, O(n) time complexity, O(n) space complexity
+export const getNthFib2 = (n: number) => {
+  const fibNums = [0, 1]
+
+  for (let i = 2; i < n; i++) {
+    fibNums.push(fibNums[i - 1] + fibNums[i - 2])
+  }
+
+  return fibNums[n - 1]
+}
