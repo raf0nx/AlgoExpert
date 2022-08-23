448bb5fdc0c6fe42757d3968ad62704a57a3ecaa
raf0nx <rafalpawlowski2011@gmail.com>
2022-08-23 22:58:03 +0200

    Refactor ProductSum algorithm solution

--- ProductSum.ts
+++ ProductSum.ts
@@ -2,15 +2,15 @@
 type SpecialArray = Array<number | SpecialArray>
 
 export function productSum(array: SpecialArray, multiplier = 1): number {
-  let sum = 0
-
-  for (const element of array) {
-    if (Array.isArray(element)) {
-      sum += productSum(element, multiplier + 1)
-    } else {
-      sum += element
-    }
-  }
-
-  return multiplier * sum
+  return (
+    multiplier *
+    array.reduce<number>(
+      (sum, current) =>
+        sum +
+        (Array.isArray(current)
+          ? productSum(current, multiplier + 1)
+          : current),
+      0
+    )
+  )
 }
