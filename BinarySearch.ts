09e9e31ebfedc0599f2dd8129ae9d27459642a8b
raf0nx <rafalpawlowski2011@gmail.com>
2022-08-24 01:05:15 +0200

    Add recursive solution to Binary Search algorithm

--- BinarySearch.ts
+++ BinarySearch.ts
@@ -15,3 +15,26 @@ export function binarySearch(array: number[], target: number): number {
 
   return -1
 }
+
+// Solution 2, O(log(n)) time complexity, O(n) space complexity
+function binarySearchHelper(
+  array: number[],
+  target: number,
+  left: number,
+  right: number
+): number {
+  if (left > right) return -1
+
+  const mid = Math.floor((left + right) / 2)
+  const potentialMatch = array[mid]
+
+  if (potentialMatch === target) return mid
+
+  if (potentialMatch > target)
+    return binarySearchHelper(array, target, left, mid - 1)
+  else return binarySearchHelper(array, target, mid + 1, right)
+}
+
+export function binarySearch2(array: number[], target: number): number {
+  return binarySearchHelper(array, target, 0, array.length - 1)
+}
