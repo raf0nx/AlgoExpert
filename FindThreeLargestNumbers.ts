ffa0e9a96bd4e96c2e6437f337fe93dc4edee878
raf0nx <rafalpawlowski2011@gmail.com>
2022-08-26 02:05:31 +0200

    Add second solution to Find Three Largest Numbers algorithm

--- FindThreeLargestNumbers.ts
+++ FindThreeLargestNumbers.ts
@@ -19,3 +19,30 @@ export function findThreeLargestNumbers(array: number[]) {
 
   return [thirdBiggestNum, secondBiggestNum, biggestNum]
 }
+
+// Solution 2, O(n) time complexity, O(1) space complexity
+function updateAndShuffleArray(array: number[], element: number, idx: number) {
+  for (let i = 0; i <= idx; i++) {
+    if (i === idx) {
+      array[i] = element
+    } else {
+      array[i] = array[i + 1]
+    }
+  }
+}
+
+export function findThreeLargestNumbers2(array: number[]) {
+  const result: Array<number> = Array(3).fill(-Infinity)
+
+  for (const number of array) {
+    if (number > result[2]) {
+      updateAndShuffleArray(result, number, 2)
+    } else if (number > result[1]) {
+      updateAndShuffleArray(result, number, 1)
+    } else if (number > result[0]) {
+      updateAndShuffleArray(result, number, 0)
+    }
+  }
+
+  return result
+}
