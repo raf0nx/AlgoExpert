d365de04f1cff7be596bbac0f5e0cf0c2d2c7d46
raf0nx <rafalpawlowski2011@gmail.com>
2022-08-29 18:12:03 +0200

    Add second solution to Insertion Sort algorithm

--- InsertionSort.ts
+++ InsertionSort.ts
@@ -13,3 +13,17 @@ export function insertionSort(array: number[]) {
 
   return array
 }
+
+// Solution 2, Best: O(n), Avg and worst: O(n^2) time complexity, O(1) space complexity
+export function insertionSort2(array: number[]) {
+  for (let i = 1; i <= array.length - 1; i++) {
+    let j = i
+
+    while (j > 0 && array[j] < array[j - 1]) {
+      ;[array[j - 1], array[j]] = [array[j], array[j - 1]]
+      j--
+    }
+  }
+
+  return array
+}
