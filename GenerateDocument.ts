7ea8d59029da60fb1fa8a1e7f3651a59232da515
raf0nx <rafalpawlowski2011@gmail.com>
2022-09-18 19:25:49 +0200

    Add solution with Map

--- GenerateDocument.ts
+++ GenerateDocument.ts
@@ -23,3 +23,29 @@ function createCharsFrequencyMap(chars: string) {
 
   return charsMap
 }
+
+/**
+ * Solution 2, O(n + m) time complexity, O(c) space complexity,
+ * where n is the length of characters, m is the length of the document and
+ * c is the number of unique characters in the characters string
+ */
+export function generateDocument2(characters: string, document: string) {
+  const charactersFrequencyMap = createCharsFrequencyMap2(characters)
+
+  for (const char of document) {
+    if (!charactersFrequencyMap.get(char)) return false
+    charactersFrequencyMap.set(char, charactersFrequencyMap.get(char)! - 1)
+  }
+
+  return true
+}
+
+function createCharsFrequencyMap2(chars: string) {
+  const charsMap = new Map<string, number>()
+
+  for (const char of chars) {
+    charsMap.set(char, (charsMap.get(char) || 0) + 1)
+  }
+
+  return charsMap
+}
