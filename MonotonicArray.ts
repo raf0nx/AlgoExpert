7f67a03abf8da55bacfda8b03e955788e133a831
raf0nx <rafalpawlowski2011@gmail.com>
2022-09-27 23:16:50 +0200

    Add alternative solution

--- MonotonicArray.ts
+++ MonotonicArray.ts
@@ -9,3 +9,16 @@ export function isMonotonic(array: number[]) {
     return isIncreasing ? num >= prevNum : num <= prevNum
   })
 }
+
+// Solution 2, O(n) time complexity, O(1) space complexity
+export function isMonotonic2(array: number[]) {
+  let isIncreasing = true
+  let isDecreasing = true
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > array[i - 1]) isDecreasing = false
+    if (array[i] < array[i - 1]) isIncreasing = false
+  }
+
+  return isIncreasing || isDecreasing
+}
