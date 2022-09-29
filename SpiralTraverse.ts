844cca4278209008822b9c6aff3c0507e3b2e157
raf0nx <rafalpawlowski2011@gmail.com>
2022-09-29 22:33:04 +0200

    Add more optimized solution without keeping track of the direction

--- SpiralTraverse.ts
+++ SpiralTraverse.ts
@@ -1,4 +1,4 @@
-// Solution 1, O(n) time complexity, O(n) space complexity
+// Solution 1, O(n) time complexity, O(n) space complexity1
 export function spiralTraverse(array: number[][]) {
   const visitedNums: number[] = []
 
@@ -48,6 +48,43 @@ export function spiralTraverse(array: number[][]) {
 
         break
     }
+  }
+
+  return visitedNums
+}
+
+// Solution 2, O(n) time complexity, O(n) space complexity1
+export function spiralTraverse2(array: number[][]) {
+  const visitedNums: number[] = []
+
+  let startRow = 0,
+    startCol = 0,
+    endRow = array.length - 1,
+    endCol = array[array.length - 1].length - 1
+
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let i = startCol; i <= endCol; i++) {
+      visitedNums.push(array[startRow][i])
+    }
+
+    for (let i = startRow + 1; i <= endRow; i++) {
+      visitedNums.push(array[i][endCol])
+    }
+
+    if (startRow === endRow || startCol === endCol) break
+
+    for (let i = endCol - 1; i >= startCol; i--) {
+      visitedNums.push(array[endRow][i])
+    }
+
+    for (let i = endRow - 1; i >= startRow + 1; i--) {
+      visitedNums.push(array[i][startCol])
+    }
+
+    startRow += 1
+    startCol += 1
+    endRow -= 1
+    endCol -= 1
   }
 
   return visitedNums
