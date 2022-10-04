1ac820949311bfcbf98c208791a42b287382dd70
raf0nx <rafalpawlowski2011@gmail.com>
2022-10-05 00:03:06 +0200

    Add second solution

--- LongestPeak.ts
+++ LongestPeak.ts
@@ -34,3 +34,42 @@ function getPeakItemsCount(
 
   return count
 }
+
+// Solution 2, O(n) time complexity, 0(1) space complexity
+export function longestPeak2(array: number[]) {
+  let currLongestPeak = 0,
+    idx = 1
+
+  while (idx < array.length - 1) {
+    if (array[idx - 1] < array[idx] && array[idx] > array[idx + 1]) {
+      const leftPeakBound = getLeftPeakBound(array, idx - 1)
+      const rightPeakBound = getRightPeakBound(array, idx + 1)
+
+      const currPeak = rightPeakBound - leftPeakBound + 1
+
+      if (currPeak > currLongestPeak) currLongestPeak = currPeak
+
+      idx = rightPeakBound - 1
+    }
+
+    idx++
+  }
+
+  return currLongestPeak
+}
+
+function getLeftPeakBound(array: number[], idx: number) {
+  let i = idx
+
+  while (i >= 1 && array[i] > array[i - 1]) i--
+
+  return i
+}
+
+function getRightPeakBound(array: number[], idx: number) {
+  let i = idx
+
+  while (i < array.length - 1 && array[i] > array[i + 1]) i++
+
+  return i
+}
