b0a8aa6d90678117735e04d4724a708f05ee2c63
raf0nx <rafalpawlowski2011@gmail.com>
2022-10-07 01:14:52 +0200

    Add more optimized solution

--- ArrayOfProducts.ts
+++ ArrayOfProducts.ts
@@ -14,3 +14,22 @@ export function arrayOfProducts(array: number[]) {
 
   return result
 }
+
+// Solution 2, O(n) time complexity, O(n) space complexity
+export function arrayOfProducts2(array: number[]) {
+  const result: number[] = []
+
+  let leftRunningProduct = 1
+  for (let i = 0; i < array.length; i++) {
+    result.push(leftRunningProduct)
+    leftRunningProduct *= array[i]
+  }
+
+  let rightRunningProduct = 1
+  for (let i = array.length - 1; i >= 0; i--) {
+    result[i] *= rightRunningProduct
+    rightRunningProduct *= array[i]
+  }
+
+  return result
+}
