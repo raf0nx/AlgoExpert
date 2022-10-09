7656eb7ae0a769030e807b90abfb419938b7f347
raf0nx <rafalpawlowski2011@gmail.com>
2022-10-09 17:25:56 +0200

    Add second, more space performant, solution

--- FirstDuplicateValue.ts
+++ FirstDuplicateValue.ts
@@ -9,3 +9,14 @@ export function firstDuplicateValue(array: number[]) {
 
   return -1
 }
+
+// Solution 2, O(n) time complexity, O(1) space complexity
+export function firstDuplicateValue2(array: number[]) {
+  for (const num of array) {
+    const indice = Math.abs(num) - 1
+    if (array[indice] < 0) return Math.abs(num)
+    array[indice] *= -1
+  }
+
+  return -1
+}
