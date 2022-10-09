5362f6810fad20e6a2e40e0c4293ff5e7d3914ab
raf0nx <rafalpawlowski2011@gmail.com>
2022-10-09 16:47:21 +0200

    Add a comment

--- FirstDuplicateValue.ts
+++ FirstDuplicateValue.ts
@@ -1,6 +1,6 @@
 // Solution 1, O(n) time complexity, O(1) space complexity
 export function firstDuplicateValue(array: number[]) {
-  const traversedNums = new Set()
+  const traversedNums = new Set() // Or use a Hash Map here
 
   for (const num of array) {
     if (traversedNums.has(num)) return num
