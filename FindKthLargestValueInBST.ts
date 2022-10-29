5012978500edc1ea82fb8875d224abb5b4d23f9c
raf0nx <rafalpawlowski2011@gmail.com>
2022-10-29 19:37:08 +0200

    Optimize solution

--- FindKthLargestValueInBST.ts
+++ FindKthLargestValueInBST.ts
@@ -26,3 +26,36 @@ export function findKthLargestValueInBst(
 
   return traversed[k - 1]
 }
+
+// Solution 2, O(h + k) time complexity, O(h) space complexity where h is the height of the tree and k is the function parameter
+type NodesInfo = {
+  nodesVisited: number
+  lastVisitedNodeValue: number
+}
+
+export function findKthLargestValueInBst2(tree: BST | null, k: number) {
+  const initNodesInfo: NodesInfo = {
+    nodesVisited: 0,
+    lastVisitedNodeValue: -Infinity,
+  }
+
+  return findKthLargestValueInBst2Helper(tree, k, initNodesInfo)
+}
+
+function findKthLargestValueInBst2Helper(
+  node: BST | null,
+  k: number,
+  nodesInfo: NodesInfo
+) {
+  if (!node || nodesInfo.nodesVisited >= k) return
+
+  findKthLargestValueInBst2Helper(node.right, k, nodesInfo)
+
+  if (nodesInfo.nodesVisited < k) {
+    nodesInfo.nodesVisited += 1
+    nodesInfo.lastVisitedNodeValue = node.value
+    findKthLargestValueInBst2Helper(node.left, k, nodesInfo)
+  }
+
+  return nodesInfo.lastVisitedNodeValue
+}
