ee318124494d6ca2f48414d89e54d4ceb43c5b8a
raf0nx <rafalpawlowski2011@gmail.com>
2022-10-31 00:28:27 +0100

    Add solution to the Reconstruct BST algorithm

---
+++ ReconstructBST.ts
@@ -0,0 +1,63 @@
+// This is an input class. Do not edit.
+export class BST {
+  value: number
+  left: BST | null
+  right: BST | null
+
+  constructor(
+    value: number,
+    left: BST | null = null,
+    right: BST | null = null
+  ) {
+    this.value = value
+    this.left = left
+    this.right = right
+  }
+}
+
+type IndexInfo = { index: number }
+
+// Solution 1, O(n + h) time complexity, O(h) space complexity where h is the height of the tree
+export function reconstructBst(preOrderTraversalValues: number[]): BST | null {
+  const indexInfo: IndexInfo = { index: 0 }
+
+  return reconstructBstHelper(
+    preOrderTraversalValues,
+    -Infinity,
+    Infinity,
+    indexInfo
+  )
+}
+
+function reconstructBstHelper(
+  preOrderTraversalValues: number[],
+  min: number,
+  max: number,
+  indexInfo: IndexInfo
+) {
+  if (indexInfo.index >= preOrderTraversalValues.length) return null
+
+  const currNodeValue = preOrderTraversalValues[indexInfo.index]
+
+  if (currNodeValue < min || currNodeValue >= max) return null
+
+  indexInfo.index += 1
+
+  const node = new BST(currNodeValue)
+
+  node.left = reconstructBstHelper(
+    preOrderTraversalValues,
+    min,
+    node.value,
+    indexInfo
+  )
+
+  node.right = reconstructBstHelper(
+    preOrderTraversalValues,
+    node.value,
+    max,
+    indexInfo
+  )
+
+  return node
+}
