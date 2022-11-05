102773163a31a22ac544e8c204b63cbf171faf42
raf0nx <rafalpawlowski2011@gmail.com>
2022-11-05 19:51:54 +0100

    Refactor solution 1

--- BinaryTreeDiameter.ts
+++ BinaryTreeDiameter.ts
@@ -15,27 +15,26 @@ type Diameter = { value: number }
 
 // Solution 1, Avg: O(n) time complexity, O(h) space complexity where h is the height of the tree, Worst: O(n) space complexity
 export function binaryTreeDiameter(tree: BinaryTree) {
-  const diameterInfo: Diameter = { value: 0 }
+  const diameter: Diameter = { value: 0 }
 
-  calculateDiameter(tree, diameterInfo)
+  calculateDiameter(tree, diameter)
 
-  return diameterInfo.value
+  return diameter.value
 }
 
-function calculateDiameter(tree: BinaryTree | null, diameterInfo: Diameter) {
-  // Reset potential edge when null node encountered
-  if (!tree) return -1
+function calculateDiameter(
+  tree: BinaryTree | null,
+  diameter: Diameter
+): number {
+  if (!tree) return 0
 
-  let leftSubtreeDiameter = 1
-  let rightSubtreeDiameter = 1
+  const leftSubtreeDiameter = calculateDiameter(tree.left, diameter)
+  const rightSubtreeDiameter = calculateDiameter(tree.right, diameter)
 
-  leftSubtreeDiameter += calculateDiameter(tree.left, diameterInfo)
-  rightSubtreeDiameter += calculateDiameter(tree.right, diameterInfo)
-
-  diameterInfo.value = Math.max(
-    diameterInfo.value,
+  diameter.value = Math.max(
+    diameter.value,
     leftSubtreeDiameter + rightSubtreeDiameter
   )
 
-  return Math.max(leftSubtreeDiameter, rightSubtreeDiameter)
+  return Math.max(leftSubtreeDiameter, rightSubtreeDiameter) + 1
 }
