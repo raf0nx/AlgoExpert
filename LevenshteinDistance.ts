bc192f054d1715d7457e2b1aea7b8b27519402f9
raf0nx <rafalpawlowski2011@gmail.com>
2022-11-20 21:28:30 +0100

    Add solution with more optimized space complexity

--- LevenshteinDistance.ts
+++ LevenshteinDistance.ts
@@ -32,3 +32,42 @@ export function levenshteinDistance(str1: string, str2: string) {
 
   return minNumsOfEdits[str2.length][str1.length]
 }
+
+// Solution 2, O(n * m) time complexity, O(min(n, m)) space complexity where n is the length of the first string and m is the length of the second string
+export function levenshteinDistance2(str1: string, str2: string) {
+  const smallerStr = str1.length < str2.length ? str1 : str2
+  const biggerStr = str1.length >= str2.length ? str1 : str2
+
+  const evenRow = Array.from<number, number>(
+    { length: smallerStr.length + 1 },
+    (_, i) => i
+  )
+  const oddRow: number[] = []
+
+  for (let row = 1; row < biggerStr.length + 1; row++) {
+    let currentRow: number[], prevRow: number[]
+
+    if (row % 2 === 0) {
+      currentRow = evenRow
+      prevRow = oddRow
+    } else {
+      currentRow = oddRow
+      prevRow = evenRow
+    }
+
+    currentRow[0] = row
+
+    for (let col = 1; col < smallerStr.length + 1; col++) {
+      if (smallerStr[col - 1] === biggerStr[row - 1]) {
+        currentRow[col] = prevRow[col - 1]
+      } else {
+        currentRow[col] =
+          1 + Math.min(currentRow[col - 1], prevRow[col], prevRow[col - 1])
+      }
+    }
+  }
+
+  return biggerStr.length % 2 === 0
+    ? evenRow[smallerStr.length]
+    : oddRow[smallerStr.length]
+}
