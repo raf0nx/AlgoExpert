cfa997effcf91d014aa479dfe8395576be69275c
raf0nx <rafalpawlowski2011@gmail.com>
2022-11-27 17:12:50 +0100

    Add cleaner solution

--- SingleCycleCheck.ts
+++ SingleCycleCheck.ts
@@ -14,6 +14,21 @@ export function hasSingleCycle(array: number[]) {
   return nextIndex === 0 && jumpBy !== 0
 }
 
+// Solution 2, O(n) time complexity, O(1) space complexity
+export function hasSingleCycle2(array: number[]) {
+  let jumpsLeft = array.length
+  let currIdx = 0
+
+  while (jumpsLeft) {
+    if (currIdx === 0 && jumpsLeft < array.length - 1) return false
+
+    currIdx = getNextIndex(currIdx, array[currIdx], array.length)
+    jumpsLeft--
+  }
+
+  return currIdx === 0
+}
+
 function getNextIndex(
   currIdx: number,
   modifier: number,
