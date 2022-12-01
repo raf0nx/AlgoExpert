a58a15cc709609720205a31c20956720a0faa439
raf0nx <rafalpawlowski2011@gmail.com>
2022-12-01 23:56:16 +0100

    Refactor solution and improve its time complexity via memoizing visited nodes

--- RiverSizes.ts
+++ RiverSizes.ts
@@ -1,26 +1,37 @@
-// Solution 1, O(wh^2) time complexity, O(wh) space complexity where w is the width and h is the height of the matrix
+// Solution 1, O(wh) time complexity, O(wh) space complexity where w is the width and h is the height of the matrix
 export function riverSizes(matrix: number[][]) {
+  const visited: boolean[][] = Array.from({ length: matrix.length }, el =>
+    new Array(matrix[0].length).fill(false)
+  )
   const riverSizes: number[] = []
 
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-      if (!matrix[i][j]) continue
-      riverSizes.push(findRiverSize(matrix, i, j))
+      const riverSize = findRiverSize(matrix, visited, i, j)
+      riverSize && riverSizes.push(riverSize)
     }
   }
 
   return riverSizes
 }
 
-function findRiverSize(matrix: number[][], row: number, col: number): number {
-  if (!matrix[row] || !matrix[row][col]) return 0
+function findRiverSize(
+  matrix: number[][],
+  visited: boolean[][],
+  row: number,
+  col: number
+): number {
+  if (!matrix[row] || visited[row][col]) return 0
 
-  matrix[row][col] = 0
+  visited[row][col] = true
+
+  if (!matrix[row][col]) return 0
+
   return (
     1 +
-    findRiverSize(matrix, row, col + 1) +
-    findRiverSize(matrix, row + 1, col) +
-    findRiverSize(matrix, row, col - 1) +
-    findRiverSize(matrix, row - 1, col)
+    findRiverSize(matrix, visited, row, col + 1) +
+    findRiverSize(matrix, visited, row + 1, col) +
+    findRiverSize(matrix, visited, row, col - 1) +
+    findRiverSize(matrix, visited, row - 1, col)
   )
 }
