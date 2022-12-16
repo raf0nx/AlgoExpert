a11e554570c1751e35950e3869d67f29b191e28d
raf0nx <rafalpawlowski2011@gmail.com>
2022-12-16 21:40:40 +0100

    Rename variables

--- MinimumPassesOfMatrix.ts
+++ MinimumPassesOfMatrix.ts
@@ -1,15 +1,15 @@
 // Solution 1, O(w * h) time complexity, O(w * h) space complexity
 export function minimumPassesOfMatrix(matrix: number[][]) {
-  const negatives = Array.from({ length: matrix.length }, (_, i) =>
+  const convertableNegatives = Array.from({ length: matrix.length }, (_, i) =>
     new Array(matrix[i].length).fill(false)
   )
 
-  return removeNegatives(matrix, negatives, 0)
+  return removeNegatives(matrix, convertableNegatives, 0)
 }
 
 function removeNegatives(
   matrix: number[][],
-  negatives: boolean[][],
+  convertableNegatives: boolean[][],
   nrOfPasses: number
 ): number {
   let negativesLeft = false
@@ -18,16 +18,16 @@ function removeNegatives(
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] <= 0) continue
-      markNegativeNeighbors(matrix, negatives, i, j)
+      markConvertableNegativeNeighbors(matrix, convertableNegatives, i, j)
     }
   }
 
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (!negativesLeft && matrix[i][j] < 0) negativesLeft = true
-      if (negatives[i][j]) {
+      if (convertableNegatives[i][j]) {
         negativeRemoved = true
-        negatives[i][j] = false
+        convertableNegatives[i][j] = false
         matrix[i][j] *= -1
       }
     }
@@ -36,12 +36,12 @@ function removeNegatives(
   if (!negativesLeft) return nrOfPasses
   if (negativesLeft && !negativeRemoved) return -1
 
-  return removeNegatives(matrix, negatives, nrOfPasses + 1)
+  return removeNegatives(matrix, convertableNegatives, nrOfPasses + 1)
 }
 
-function markNegativeNeighbors(
+function markConvertableNegativeNeighbors(
   matrix: number[][],
-  negatives: boolean[][],
+  convertableNegatives: boolean[][],
   row: number,
   col: number
 ) {
@@ -49,10 +49,10 @@ function markNegativeNeighbors(
     if (
       isOutOfBound(matrix, nRow, nCol) ||
       matrix[nRow][nCol] >= 0 ||
-      negatives[nRow][nCol]
+      convertableNegatives[nRow][nCol]
     )
       continue
-    negatives[nRow][nCol] = true
+    convertableNegatives[nRow][nCol] = true
   }
 }
 
