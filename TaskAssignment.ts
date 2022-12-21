4acda1ce725808d133e327cf200a6c1932ae1020
raf0nx <rafalpawlowski2011@gmail.com>
2022-12-21 23:10:06 +0100

    Add "one-liner" solution

--- TaskAssignment.ts
+++ TaskAssignment.ts
@@ -15,3 +15,20 @@ export function taskAssignment(k: number, tasks: number[]) {
 
   return assignedTasks
 }
+
+// Solution 2, O(n * log(n)) time complexity, O(n) space complexity where n is the number of tasks
+export function taskAssignment2(k: number, tasks: number[]) {
+  return tasks
+    .map((_, i) => i)
+    .sort((a, b) => tasks[a] - tasks[b])
+    .reduce<number[][]>((assignedTasks, currTask, i, sortedTasksIdxs) => {
+      if (i >= k) return assignedTasks
+
+      assignedTasks.push([
+        currTask,
+        sortedTasksIdxs[sortedTasksIdxs.length - 1 - i],
+      ])
+
+      return assignedTasks
+    }, [])
+}
