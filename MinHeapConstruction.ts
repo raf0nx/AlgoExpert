368e6ca252e6f611f80aa542c2db1c70370e82cc
raf0nx <rafalpawlowski2011@gmail.com>
2022-12-25 17:28:13 +0100

    Add Min Heap class definition

---
+++ MinHeapConstruction.ts
@@ -0,0 +1,34 @@
+export class MinHeap {
+  heap: number[]
+
+  constructor(array: number[]) {
+    this.heap = this.buildHeap(array)
+  }
+
+  buildHeap(array: number[]) {
+    // Write your code here.
+    return array
+  }
+
+  siftDown() {
+    // Write your code here.
+  }
+
+  siftUp() {
+    // Write your code here.
+  }
+
+  peek() {
+    // Write your code here.
+    return -1
+  }
+
+  remove() {
+    // Write your code here.
+    return -1
+  }
+
+  insert(value: number) {
+    // Write your code here.
+  }
+}
