a1c4af368e27736d9ef6768d902a318baf2905bf
raf0nx <rafalpawlowski2011@gmail.com>
2022-12-25 17:55:54 +0100

    Implement 'buildHeap' and 'siftDown' methods

--- MinHeapConstruction.ts
+++ MinHeapConstruction.ts
@@ -6,12 +6,41 @@ export class MinHeap {
   }
 
   buildHeap(array: number[]) {
-    // Write your code here.
+    for (let i = array.length - 1; i >= 0; i--) {
+      this.siftDown(array, i)
+    }
+
     return array
   }
 
-  siftDown() {
-    // Write your code here.
+  siftDown(heap: number[], i: number) {
+    while (i < heap.length) {
+      const child1 = i * 2 + 1
+      const child2 = i * 2 + 2
+
+      if (!child1 && !child2) return
+
+      if (child1 && !child2) {
+        if (heap[i] > heap[child1]) this.swapItemsInArray(heap, i, child1)
+
+        i = child1
+        continue
+      }
+
+      if (!child1 && child2) {
+        if (heap[i] > heap[child2]) this.swapItemsInArray(heap, i, child2)
+
+        i = child2
+        continue
+      }
+
+      const smallerChild = heap[child1] < heap[child2] ? child1 : child2
+
+      if (heap[i] > heap[smallerChild])
+        this.swapItemsInArray(heap, i, smallerChild)
+
+      i = smallerChild
+    }
   }
 
   siftUp() {
@@ -30,5 +59,9 @@ export class MinHeap {
 
   insert(value: number) {
     // Write your code here.
+  }
+
+  swapItemsInArray(array: number[], idx1: number, idx2: number) {
+    [array[idx1], array[idx2]] = [array[idx2], array[idx1]]
   }
 }
