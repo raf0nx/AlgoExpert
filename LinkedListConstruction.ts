d3864150bc5ad1ea4dac1bad3a31eab51026e614
raf0nx <rafalpawlowski2011@gmail.com>
2022-12-27 21:38:21 +0100

    Add DoublyLinkedList class definition

---
+++ LinkedListConstruction.ts
@@ -0,0 +1,54 @@
+export class Node {
+  value: number
+  prev: Node | null
+  next: Node | null
+
+  constructor(value: number) {
+    this.value = value
+    this.prev = null
+    this.next = null
+  }
+}
+
+export class DoublyLinkedList {
+  head: Node | null
+  tail: Node | null
+
+  constructor() {
+    this.head = null
+    this.tail = null
+  }
+
+  setHead(node: Node) {
+    // Write your code here.
+  }
+
+  setTail(node: Node) {
+    // Write your code here.
+  }
+
+  insertBefore(node: Node, nodeToInsert: Node) {
+    // Write your code here.
+  }
+
+  insertAfter(node: Node, nodeToInsert: Node) {
+    // Write your code here.
+  }
+
+  insertAtPosition(position: number, nodeToInsert: Node) {
+    // Write your code here.
+  }
+
+  removeNodesWithValue(value: number) {
+    // Write your code here.
+  }
+
+  remove(node: Node) {
+    // Write your code here.
+  }
+
+  containsNodeWithValue(value: number) {
+    // Write your code here.
+    return false
+  }
+}
