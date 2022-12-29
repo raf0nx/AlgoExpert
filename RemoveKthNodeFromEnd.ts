c20e19da703828599339a054375c570335a1f71c
raf0nx <rafalpawlowski2011@gmail.com>
2022-12-29 16:24:31 +0100

    Add another solution

--- RemoveKthNodeFromEnd.ts
+++ RemoveKthNodeFromEnd.ts
@@ -46,3 +46,29 @@ function removeNode(node: LinkedList | null) {
   node.value = node.next.value
   node.next = node.next.next
 }
+
+// Solution 2, O(n) time complexity, O(1) space complexity
+export function removeKthNodeFromEnd2(head: LinkedList, k: number) {
+  let node1 = head
+  let node2: LinkedList | null = head
+  let count = 0
+
+  while (count < k) {
+    node2 = node2.next!
+    count++
+  }
+
+  if (!node2) {
+    head.value = head.next!.value
+    head.next = head.next!.next
+
+    return
+  }
+
+  while (node2.next) {
+    node2 = node2.next
+    node1 = node1.next!
+  }
+
+  node1.next = node1.next!.next
+}
