4d9275ad7b24faeca116b5105670258e6c76a22f
raf0nx <rafalpawlowski2011@gmail.com>
2023-01-04 23:45:05 +0100

    Add more optimal solution

--- MergingLinkedLists.ts
+++ MergingLinkedLists.ts
@@ -33,3 +33,46 @@ export function mergingLinkedLists(
 
   return null
 }
+
+// Solution 2, O(n + m) time complexity, O(1) space complexity, where n is the length of the first linked list and m is the length of the second linked list
+export function mergingLinkedLists2(
+  linkedListOne: LinkedList,
+  linkedListTwo: LinkedList
+) {
+  let ll1Length = getLinkedListLength(linkedListOne)
+  let ll2Length = getLinkedListLength(linkedListTwo)
+
+  let ll1Node: LinkedList | null = linkedListOne
+  let ll2Node: LinkedList | null = linkedListTwo
+
+  while (ll1Length !== ll2Length) {
+    if (ll1Length > ll2Length) {
+      ll1Node = ll1Node.next!
+      ll1Length--
+    } else {
+      ll2Node = ll2Node.next!
+      ll2Length--
+    }
+  }
+
+  while (ll1Node && ll2Node) {
+    if (ll1Node === ll2Node) return ll1Node
+
+    ll1Node = ll1Node.next
+    ll2Node = ll2Node.next
+  }
+
+  return null
+}
+
+function getLinkedListLength(linkedList: LinkedList) {
+  let currNode: LinkedList | null = linkedList
+  let length = 0
+
+  while (currNode) {
+    length++
+    currNode = currNode.next
+  }
+
+  return length
+}
