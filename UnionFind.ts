f8c6531f26a3c02551205ca4ff72c0195e36b714
raf0nx <rafalpawlowski2011@gmail.com>
2023-01-15 22:16:14 +0100

    Optimize the solution

--- UnionFind.ts
+++ UnionFind.ts
@@ -1,17 +1,20 @@
 // Soltuion 1
 export class UnionFind {
   parents: { [key: number]: number }
+  ranks: { [key: number]: number }
 
   constructor() {
     this.parents = {}
+    this.ranks = {}
   }
 
   // O(1) time complexity, O(1) space complexity
   createSet(value: number) {
     this.parents[value] = value
+    this.ranks[value] = 0
   }
 
-  // O(n) time complexity, O(1) space complexity, where n is the total number of values
+  // O(log(n)) time complexity, O(1) space complexity, where n is the total number of values
   find(value: number) {
     if (!(value in this.parents)) return null
 
@@ -22,7 +25,7 @@ export class UnionFind {
     return value
   }
 
-  // O(n) time complexity, O(1) space complexity, where n is the total number of values
+  // O(log(n)) time complexity, O(1) space complexity, where n is the total number of values
   union(valueOne: number, valueTwo: number) {
     const val1Parent = this.find(valueOne)
     const val2Parent = this.find(valueTwo)
@@ -30,6 +33,13 @@ export class UnionFind {
     if (val1Parent === null || val2Parent === null || val1Parent === val2Parent)
       return
 
-    this.parents[val2Parent] = val1Parent
+    if (this.ranks[val1Parent] > this.ranks[val2Parent]) {
+      this.parents[val2Parent] = val1Parent
+    } else if (this.ranks[val1Parent] < this.ranks[val2Parent]) {
+      this.parents[val1Parent] = val2Parent
+    } else {
+      this.parents[val2Parent] = val1Parent
+      this.ranks[val1Parent] += 1
+    }
   }
 }
