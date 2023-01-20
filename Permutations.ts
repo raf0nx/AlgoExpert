32b4e96983ca627fc454d085e592dae2aa705ff8
raf0nx <rafalpawlowski2011@gmail.com>
2023-01-20 22:06:46 +0100

    Add more optimal solution

--- Permutations.ts
+++ Permutations.ts
@@ -22,3 +22,33 @@ function getPermutationsHelper(
     }
   }
 }
+
+// Solution 2, O(n! * n) time complexity, O(n! * n) space complexity
+export function getPermutations2(array: number[]) {
+  const permutations: number[][] = []
+
+  getPermutationsHelper2(array, 0, permutations)
+
+  return permutations
+}
+
+function getPermutationsHelper2(
+  array: number[],
+  currStart: number,
+  perms: number[][]
+) {
+  if (currStart && currStart === array.length) perms.push([...array])
+  else {
+    for (let i = currStart; i < array.length; i++) {
+      swapItemsInArray(array, currStart, i)
+
+      getPermutationsHelper2(array, currStart + 1, perms)
+
+      swapItemsInArray(array, currStart, i)
+    }
+  }
+}
+
+function swapItemsInArray(array: number[], idx1: number, idx2: number) {
+  ;[array[idx1], array[idx2]] = [array[idx2], array[idx1]]
+}
