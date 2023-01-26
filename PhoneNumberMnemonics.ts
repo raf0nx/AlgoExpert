65ded0faa1264dc1eda0fe5fbc0749db4563037f
raf0nx <rafalpawlowski2011@gmail.com>
2023-01-26 22:17:05 +0100

    Add another recursive solution

--- PhoneNumberMnemonics.ts
+++ PhoneNumberMnemonics.ts
@@ -40,3 +40,52 @@ function generateMnemonics(
     return generateMnemonics(phoneNumber, digitToLetters, idx + 1, newMnemons)
   }
 }
+
+// Solution 2, O(4^n * n) time complexity, O(4^n * n) space complexity
+type DigitToLetters = Record<number, string[]>
+
+export function phoneNumberMnemonics2(phoneNumber: string) {
+  const mnemonics: string[] = []
+  const digitToLetters: DigitToLetters = {
+    0: ['0'],
+    1: ['1'],
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+  }
+  const initialMnemon = new Array(phoneNumber.length).fill('0')
+
+  generateMnemonics2(phoneNumber, digitToLetters, 0, initialMnemon, mnemonics)
+
+  return mnemonics
+}
+
+function generateMnemonics2(
+  phoneNumber: string,
+  digitToLetters: DigitToLetters,
+  idx: number,
+  mnemon: string[],
+  mnemonics: string[]
+) {
+  if (idx >= phoneNumber.length) mnemonics.push(mnemon.join(''))
+  else {
+    const currNum = +phoneNumber[idx]
+    const currMnemon = [...mnemon]
+
+    for (const char of digitToLetters[currNum]) {
+      currMnemon[idx] = char
+      generateMnemonics2(
+        phoneNumber,
+        digitToLetters,
+        idx + 1,
+        currMnemon,
+        mnemonics
+      )
+    }
+  }
+}
