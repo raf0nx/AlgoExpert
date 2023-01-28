7c2d2dd5ee2131ac9788426cb8bf937e7c1533b2
raf0nx <rafalpawlowski2011@gmail.com>
2023-01-28 01:04:36 +0100

    Optimize solution 1

--- StaircaseTraversal.ts
+++ StaircaseTraversal.ts
@@ -1,12 +1,20 @@
-// Solution 1, O(k^n) time complexity, O(n) space complexity, where k is the maxSteps and n is the height
-export function staircaseTraversal(height: number, maxSteps: number): number {
-  if (height <= 1) return 1
+// Solution 1, O(k * n) time complexity, O(n) space complexity, where k is the maxSteps and n is the height
+type StaircaseTraversalMemo = { [height: number]: number }
+
+export function staircaseTraversal(
+  height: number,
+  maxSteps: number,
+  memo: StaircaseTraversalMemo = { 0: 1, 1: 1 }
+): number {
+  if (height in memo) return memo[height]
 
   let numberOfWays = 0
 
   for (let i = 1; i <= Math.min(maxSteps, height); i++) {
     numberOfWays += staircaseTraversal(height - i, maxSteps)
   }
+
+  memo[height] = numberOfWays
 
   return numberOfWays
 }
