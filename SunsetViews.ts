20dc38ca244853e3ef42efe043c1bca275b57902
raf0nx <rafalpawlowski2011@gmail.com>
2023-02-15 23:01:44 +0100

    Add solution leveraging the stack

--- SunsetViews.ts
+++ SunsetViews.ts
@@ -3,6 +3,10 @@ export enum Direction {
   West = 'WEST',
 }
 
+function isEast(direction: Direction) {
+  return direction === Direction.East
+}
+
 // Solution 1, O(n) time complexity, O(n) space complexity
 export function sunsetViews(buildings: number[], direction: Direction) {
   const result: number[] = []
@@ -26,6 +30,24 @@ export function sunsetViews(buildings: number[], direction: Direction) {
   return isEast(direction) ? result.reverse() : result
 }
 
-function isEast(direction: Direction) {
-  return direction === Direction.East
+// Solution 2, O(n) time complexity, O(n) space complexity
+export function sunsetViews2(buildings: number[], direction: Direction) {
+  const stack: number[] = []
+  const startIdx = isEast(direction) ? 0 : buildings.length - 1
+  const step = isEast(direction) ? 1 : -1
+
+  let idx = startIdx
+
+  while (idx >= 0 && idx < buildings.length) {
+    const currBuilding = buildings[idx]
+
+    while (stack.length && buildings[stack[stack.length - 1]] <= currBuilding) {
+      stack.pop()
+    }
+
+    stack.push(idx)
+    idx += step
+  }
+
+  return isEast(direction) ? stack : stack.reverse()
 }
