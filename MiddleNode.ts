6c1ca5cf28539a0d7a3a7bbf0f8f7dc64a98771e
raf0nx <rafalpawlowski2011@gmail.com>
2023-06-22 00:28:30 +0200

    Add solution to the `MiddleNode` algorithm

---
+++ MiddleNode.ts
@@ -0,0 +1,33 @@
+export class LinkedList {
+  value: number
+  next: LinkedList | null
+
+  constructor(value: number) {
+    this.value = value
+    this.next = null
+  }
+}
+
+// Solution 1, O(n) time complexity, O(n) space complexity
+// where n is the number of nodes in the linked list
+export function middleNode(linkedList: LinkedList) {
+  const linkedListLength = getLinkedListLength(linkedList)
+  const middleNodeNumber = Math.floor(linkedListLength / 2)
+
+  return retrieveMiddleNode(linkedList, middleNodeNumber)
+}
+
+function getLinkedListLength(linkedList: LinkedList): number {
+  if (!linkedList.next) return 1
+
+  return 1 + getLinkedListLength(linkedList.next)
+}
+
+function retrieveMiddleNode(
+  linkedList: LinkedList,
+  nodesToTravel: number
+): LinkedList {
+  if (nodesToTravel === 0) return linkedList
+
+  return retrieveMiddleNode(linkedList.next!, nodesToTravel - 1)
+}
