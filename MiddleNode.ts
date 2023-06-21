abe918bd91935a460a0ce78af0d68ad5ad51599b
raf0nx <rafalpawlowski2011@gmail.com>
2023-06-22 00:47:23 +0200

    Add more optimal solution to the `MiddleNode` algorithm

--- MiddleNode.ts
+++ MiddleNode.ts
@@ -31,3 +31,34 @@ function retrieveMiddleNode(
 
   return retrieveMiddleNode(linkedList.next!, nodesToTravel - 1)
 }
+
+// Solution 2, O(n) time complexity, O(1) space complexity
+// where n is the number of nodes in the linked list
+export function middleNode2(linkedList: LinkedList) {
+  const linkedListLength = getLinkedListLength2(linkedList)
+  const middleNodeNumber = Math.floor(linkedListLength / 2)
+
+  return retrieveMiddleNode2(linkedList, middleNodeNumber)
+}
+
+function getLinkedListLength2(linkedList: LinkedList) {
+  let numberOfNodes = 1
+  let currentNode = linkedList
+
+  while (currentNode.next) {
+    numberOfNodes += 1
+    currentNode = currentNode.next
+  }
+
+  return numberOfNodes
+}
+
+function retrieveMiddleNode2(linkedList: LinkedList, middleNodeNumber: number) {
+  let middleNode = linkedList
+
+  for (let i = 0; i < middleNodeNumber; i++) {
+    middleNode = middleNode.next!
+  }
+
+  return middleNode
+}
