cff15fe737d1627adf794bfebcfbb21719e7fff9
raf0nx <rafalpawlowski2011@gmail.com>
2023-06-28 02:00:19 +0200

    Add more optimal solution to the `Longest Palindromic Substring` algorithm

--- LongestPalindromicSubstring.ts
+++ LongestPalindromicSubstring.ts
@@ -34,3 +34,40 @@ function isPalindrome(string: string) {
 
   return true
 }
+
+// Solution 2, O(n^2) time complexity, O(n) space complexity
+// where n is the length of the string
+export function longestPalindromicSubstring2(string: string) {
+  let currentLongest = ''
+
+  for (let i = 0; i < string.length; i++) {
+    // Need to loop two times since palindrome can be either even or odd
+    for (let j = 0; j < 2; j++) {
+      const [leftIdx, rightIdx] = getPalindromeBoundaries(string, i, i + j)
+      const possibleLongestPalindrome = string.slice(leftIdx, rightIdx + 1)
+
+      if (possibleLongestPalindrome.length > currentLongest.length) {
+        currentLongest = possibleLongestPalindrome
+      }
+    }
+  }
+
+  return currentLongest
+}
+
+function getPalindromeBoundaries(
+  string: string,
+  leftIdx: number,
+  rightIdx: number
+) {
+  while (
+    leftIdx >= 0 &&
+    rightIdx < string.length &&
+    string[leftIdx] === string[rightIdx]
+  ) {
+    leftIdx--
+    rightIdx++
+  }
+
+  return [leftIdx + 1, rightIdx - 1]
+}
