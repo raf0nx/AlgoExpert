dc7905313798f909f3ab0fd56228254e947c285a
raf0nx <rafalpawlowski2011@gmail.com>
2023-07-14 21:39:16 +0200

    Add second solution to the `Best Seat` algorithm

--- BestSeat.ts
+++ BestSeat.ts
@@ -25,3 +25,31 @@ export function bestSeat(seats: number[]) {
 
   return bestSeatIdx
 }
+
+// Solution 2, O(n) time complexity, O(1) space complexity
+// where n is the number of seats
+export function bestSeat2(seats: number[]) {
+  let bestSeat = -1
+  let maxSpace = 0
+  let left = 0
+  let right = 1
+
+  while (right < seats.length) {
+    if (seats[right] === 0) {
+      right++
+      continue
+    }
+
+    const availableSpace = right - left - 1
+
+    if (availableSpace > maxSpace) {
+      maxSpace = availableSpace
+      bestSeat = Math.floor((left + right) / 2)
+    }
+
+    left = right
+    right++
+  }
+
+  return bestSeat
+}
