e2ad2c8c6e0fc82751d7d8a506267e8420d70d7c
raf0nx <rafalpawlowski2011@gmail.com>
2023-07-15 19:35:53 +0200

    Add the most optimal solution for the `Missing Numbers` algorithm

--- MissingNumbers.ts
+++ MissingNumbers.ts
@@ -36,3 +36,42 @@ export function missingNumbers2(nums: number[]) {
 
   return missingNums
 }
+
+// Solution 3, O(n) time complexity, O(1) space complexity
+// where n is the length of the nums array
+export function missingNumbers3(nums: number[]) {
+  const numsSum = nums.reduce((a, b) => a + b, 0)
+  const expectedSum = caluculateSumInRange(1, nums.length + 2)
+  const averageMissingValue = Math.floor((expectedSum - numsSum) / 2)
+  const expectedLeftHalfSum = caluculateSumInRange(1, averageMissingValue)
+  const expectedRightHalfSum = caluculateSumInRange(
+    averageMissingValue + 1,
+    nums.length + 2
+  )
+
+  let foundLeftHalfSum = 0
+  let foundRightHalfSum = 0
+
+  for (const num of nums) {
+    if (num <= averageMissingValue) {
+      foundLeftHalfSum += num
+    } else {
+      foundRightHalfSum += num
+    }
+  }
+
+  return [
+    expectedLeftHalfSum - foundLeftHalfSum,
+    expectedRightHalfSum - foundRightHalfSum,
+  ]
+}
+
+function caluculateSumInRange(begin: number, end: number) {
+  let sum = 0
+
+  for (let i = begin; i <= end; i++) {
+    sum += i
+  }
+
+  return sum
+}
