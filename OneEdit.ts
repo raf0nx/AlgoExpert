838bef65431d9a49c614937727099863eb14341d
raf0nx <rafalpawlowski2011@gmail.com>
2023-07-19 00:16:36 +0200

    Optimize solution 1 for the `One Edit` algorithm

--- OneEdit.ts
+++ OneEdit.ts
@@ -1,27 +1,43 @@
-// Solution 1, O(n) time complexity, O(n) space complexity
+// Solution 1, O(n) time complexity, O(1) space complexity
 // where n is the length of the longer string
 export function oneEdit(stringOne: string, stringTwo: string) {
-  const longerString =
-    stringOne.length > stringTwo.length ? stringOne : stringTwo
-  const shorterString =
-    stringOne.length > stringTwo.length ? stringTwo : stringOne
+  const lengthOne = stringOne.length
+  const lengthTwo = stringTwo.length
+
+  if (Math.abs(lengthOne - lengthTwo) > 1) {
+    return false
+  }
 
   let charsDifferenceCount = 0
-  let longerStrPointer = 0
-  let shorterStrPointer = 0
+  let onePointer = 0
+  let twoPointer = 0
 
-  while (longerStrPointer < longerString.length) {
-    if (longerString[longerStrPointer] !== shorterString[shorterStrPointer]) {
+  while (onePointer < lengthOne && twoPointer < lengthTwo) {
+    if (stringOne[onePointer] !== stringTwo[twoPointer]) {
       charsDifferenceCount += 1
-      longerStrPointer += 1
 
-      if (longerString.length === shorterString.length) shorterStrPointer += 1
+      if (charsDifferenceCount > 1) {
+        return false
+      }
+
+      if (lengthOne === lengthTwo) {
+        onePointer += 1
+        twoPointer += 1
+      } else if (lengthOne > lengthTwo) {
+        onePointer += 1
+      } else {
+        twoPointer += 1
+      }
 
       continue
     }
 
-    longerStrPointer += 1
-    shorterStrPointer += 1
+    onePointer += 1
+    twoPointer += 1
+  }
+
+  if (onePointer < lengthOne || twoPointer < lengthTwo) {
+    charsDifferenceCount += 1
   }
 
   return charsDifferenceCount <= 1
