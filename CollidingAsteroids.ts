6a1e285b68756a406b2022ad0bfb405d8d16c0ef
raf0nx <rafalpawlowski2011@gmail.com>
2023-07-27 01:33:09 +0200

    Add alternative solution to the `Colliding Asteroids` algorithm

--- CollidingAsteroids.ts
+++ CollidingAsteroids.ts
@@ -25,3 +25,31 @@ export function collidingAsteroids(asteroids: number[]) {
 
   return result.concat(stack)
 }
+
+// Solution 2, O(n) time complexity, O(n) space complexity
+// where n is the number of asteroids
+export function collidingAsteroids2(asteroids: number[]) {
+  const stack: number[] = []
+
+  for (const asteroid of asteroids) {
+    if (asteroid < 0) {
+      while (true) {
+        const topOfStack = stack[stack.length - 1]
+
+        if (!stack.length || topOfStack < 0) {
+          stack.push(asteroid)
+          break
+        } else if (Math.abs(asteroid) > Math.abs(topOfStack)) {
+          stack.pop()
+        } else if (Math.abs(asteroid) === Math.abs(topOfStack)) {
+          stack.pop()
+          break
+        } else break
+      }
+    } else {
+      stack.push(asteroid)
+    }
+  }
+
+  return stack
+}
