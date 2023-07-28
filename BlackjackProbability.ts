b2e6f4574f42b9bc9f9b5a5c20ccc774fe086513
raf0nx <rafalpawlowski2011@gmail.com>
2023-07-28 16:59:12 +0200

    Add alternative solution to the `Blackjack Probability` algorithm

--- BlackjackProbability.ts
+++ BlackjackProbability.ts
@@ -1,5 +1,13 @@
 const cards = Array.from({ length: 10 }, (_, i) => i + 1)
 
+function checkIfBust(target: number, startingHand: number) {
+  return startingHand > target
+}
+
+function checkIfStand(target: number, startingHand: number) {
+  return startingHand >= target - 4 && startingHand <= target
+}
+
 // Solution 1, O(t - s) time complexity, O(t - s) space complexity
 // where t is the target and s is the starting hand
 export function blackjackProbability(
@@ -31,10 +39,24 @@ export function blackjackProbability(
   return +result.toFixed(3)
 }
 
-function checkIfBust(target: number, startingHand: number) {
-  return startingHand > target
-}
+// Solution 2, O(t - s) time complexity, O(t - s) space complexity
+// where t is the target and s is the starting hand
+export function blackjackProbability2(
+  target: number,
+  startingHand: number,
+  memo: Record<number, number> = {}
+) {
+  if (memo[startingHand]) return memo[startingHand]
+  if (checkIfBust(target, startingHand)) return 1
+  if (checkIfStand(target, startingHand)) return 0
 
-function checkIfStand(target: number, startingHand: number) {
-  return startingHand >= target - 4 && startingHand <= target
+  let result = 0
+
+  for (let drawn = 1; drawn <= 10; drawn++) {
+    result += 0.1 * blackjackProbability(target, startingHand + drawn, memo)
+  }
+
+  memo[startingHand] = result
+
+  return result
 }
