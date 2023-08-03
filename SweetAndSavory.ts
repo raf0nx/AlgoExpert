2f1d8f841ae6797b1ddcf4c6ac264bba5666f0cc
raf0nx <rafalpawlowski2011@gmail.com>
2023-08-04 00:40:25 +0200

    Add more optimal solution to the `Sweet And Savory` algorithm

--- SweetAndSavory.ts
+++ SweetAndSavory.ts
@@ -38,3 +38,35 @@ export function sweetAndSavory(dishes: number[], target: number) {
 
   return res
 }
+
+// Solution 2, O(n * log(n)) time complexity, O(1) space complexity
+// where n is the length of the dishes array
+export function sweetAndSavory2(dishes: number[], target: number) {
+  dishes.sort((a, b) => a - b)
+
+  let res: [number, number] = [0, 0]
+  let bestFlavorTargetDiff = Infinity
+  let left = 0
+  let right = dishes.length - 1
+
+  while (dishes[left] < 0 && dishes[right] > 0) {
+    const currentFlavorSum = dishes[left] + dishes[right]
+    const currentFlavorTargetDiff = target - currentFlavorSum
+
+    if (
+      currentFlavorTargetDiff < bestFlavorTargetDiff &&
+      currentFlavorSum <= target
+    ) {
+      bestFlavorTargetDiff = currentFlavorTargetDiff
+      res = [dishes[left], dishes[right]]
+    }
+
+    if (currentFlavorSum < target) {
+      left++
+    } else {
+      right--
+    }
+  }
+
+  return res
+}
