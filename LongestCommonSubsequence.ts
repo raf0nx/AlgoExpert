f9bd9f9f41e0e178a27927f336b1e218ff761606
raf0nx <rafalpawlowski2011@gmail.com>
2023-10-06 01:52:05 +0200

    Add the most optimal solution to the `Longest Common Subsequence` algorithm

--- LongestCommonSubsequence.ts
+++ LongestCommonSubsequence.ts
@@ -57,3 +57,53 @@ export function longestCommonSubsequence2(str1: string, str2: string) {
     ? evenRow[small.length].split('')
     : oddRow[small.length].split('')
 }
+
+// Solution 3, O(n * m) time complexity, O(n * m) space complexity
+// where n and m are the lengths of the two input strings
+type LetterUsedInLcs = string | null
+type LcsLength = number
+type PreviousIIdx = number | null
+type PreviousJIdx = number | null
+type LCSEntry = [LetterUsedInLcs, LcsLength, PreviousIIdx, PreviousJIdx]
+type LCS = LCSEntry[][]
+
+export function longestCommonSubsequence3(str1: string, str2: string) {
+  const lcs: LCS = Array.from({ length: str2.length + 1 }, () =>
+    new Array(str1.length + 1).fill([null, 0, null, null])
+  )
+
+  for (let i = 1; i < str2.length + 1; i++) {
+    for (let j = 1; j < str1.length + 1; j++) {
+      if (str2[i - 1] === str1[j - 1]) {
+        lcs[i][j] = [str2[i - 1], lcs[i - 1][j - 1][1] + 1, i - 1, j - 1]
+      } else {
+        if (lcs[i - 1][j][1] > lcs[i][j - 1][1]) {
+          lcs[i][j] = [null, lcs[i - 1][j][1], i - 1, j]
+        } else {
+          lcs[i][j] = [null, lcs[i][j - 1][1], i, j - 1]
+        }
+      }
+    }
+  }
+
+  return buildSequence(lcs)
+}
+
+function buildSequence(lcs: LCS) {
+  const sequence: string[] = []
+  let i: number | null = lcs.length - 1
+  let j: number | null = lcs[0].length - 1
+
+  while (i && j) {
+    const currentEntry: LCSEntry = lcs[i][j]
+
+    if (currentEntry[0]) {
+      sequence.push(currentEntry[0])
+    }
+
+    i = currentEntry[2]
+    j = currentEntry[3]
+  }
+
+  return sequence.reverse()
+}
