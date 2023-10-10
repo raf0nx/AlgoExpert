da810391cd51e9c7cb320ad23b8b3a650ebff81f
raf0nx <rafalpawlowski2011@gmail.com>
2023-10-11 00:34:12 +0200

    Add second solution to the `Min Number of Jumps` algorithm

--- MinNumberOfJumps.ts
+++ MinNumberOfJumps.ts
@@ -1,4 +1,4 @@
-// Solution 1, O(n) time complexity, O(1) space complexity
+// Solution 1, O(n) time complexity, O(1) space complexity,
 // where n is the length of the input array
 export function minNumberOfJumps(array: number[]) {
   let numOfJumps = 0
@@ -25,4 +25,21 @@ export function minNumberOfJumps(array: number[]) {
   }
 
   return numOfJumps
+}
+
+// Solution 2, O(n^2) time complexity, O(n) space complexity,
+// where n is the length of the input array
+export function minNumberOfJumps2(array: number[]) {
+  const jumps: number[] = new Array(array.length).fill(Infinity)
+  jumps[0] = 0
+
+  for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] + j >= i) {
+        jumps[i] = Math.min(jumps[i], jumps[j] + 1)
+      }
+    }
+  }
+
+  return jumps[jumps.length - 1]
 }
