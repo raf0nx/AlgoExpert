90089f93ef3f50bc0c48a057ac9c745e6d49d4c9
raf0nx <rafalpawlowski2011@gmail.com>
2023-10-17 23:54:18 +0200

    Add optimal solution to the `Knapsack Problem` algorithm

--- KnapsackProblem.ts
+++ KnapsackProblem.ts
@@ -68,3 +68,45 @@ function findMaxCombinedValue(
 
   return [max === -Infinity ? 0 : max, maxCombinationIdxs]
 }
+
+// Solution 2, O(n * c) time complexity, O(n * c) space complexity,
+// where n is the number of items and c is the capacity
+export function knapsackProblem2(
+  items: [number, number][],
+  capacity: number
+): [number, number[]] {
+  const maxValues = Array.from<number, number[]>(
+    { length: items.length + 1 },
+    () => new Array(capacity + 1).fill(0)
+  )
+
+  for (let i = 1; i < maxValues.length; i++) {
+    for (let j = 1; j <= capacity; j++) {
+      const [value, weight] = items[i - 1]
+      const currentAndPreviousMaxSum =
+        weight > j ? 0 : value + maxValues[i - 1][j - weight]
+
+      maxValues[i][j] = Math.max(maxValues[i - 1][j], currentAndPreviousMaxSum)
+    }
+  }
+
+  return [maxValues[items.length][capacity], getKnapsackItems(items, maxValues)]
+}
+
+function getKnapsackItems(items: [number, number][], maxValues: number[][]) {
+  const itemsIdxs: number[] = []
+  let currentPosition = [maxValues.length - 1, maxValues[0].length - 1]
+
+  while (currentPosition[0] > 0 && currentPosition[1] > 0) {
+    const [row, col] = currentPosition
+
+    if (maxValues[row][col] === maxValues[row - 1][col]) {
+      currentPosition = [row - 1, col]
+    } else {
+      itemsIdxs.push(row - 1)
+      currentPosition = [row - 1, col - items[row - 1][1]]
+    }
+  }
+
+  return itemsIdxs.reverse()
+}
