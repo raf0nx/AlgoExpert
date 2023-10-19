8a87294cc672a6e75c8a1e74754d4815ec97285e
raf0nx <rafalpawlowski2011@gmail.com>
2023-10-19 22:02:50 +0200

    Add solution to the `Disk Stacking` algorithm

---
+++ DiskStacking.ts
@@ -0,0 +1,50 @@
+// Solution 1, O(n^2) time complexity, O(n) space complexity
+// where n is the number of disks
+type Disk = [number, number, number]
+type PrevDiskIndex = number | null
+
+export function diskStacking(disks: Disk[]) {
+  disks.sort(([widthA], [widthB]) => widthA - widthB)
+
+  const heights: number[] = new Array(disks.length).fill(0)
+  const indexes: PrevDiskIndex[] = new Array(disks.length).fill(null)
+  let maxHeightIdx = 0
+
+  for (let i = 0; i < disks.length; i++) {
+    const [width, depth, height] = disks[i]
+
+    heights[i] = height
+
+    for (let j = 0; j < i; j++) {
+      const [currWidth, currDepth, currHeight] = disks[j]
+
+      if (currWidth >= width || currDepth >= depth || currHeight >= height)
+        continue
+
+      if (height + heights[j] > heights[i]) {
+        heights[i] = height + heights[j]
+        indexes[i] = j
+      }
+    }
+
+    if (heights[i] > heights[maxHeightIdx]) maxHeightIdx = i
+  }
+
+  return buildDiskStack(disks, indexes, maxHeightIdx)
+}
+
+function buildDiskStack(
+  disks: Disk[],
+  indexes: PrevDiskIndex[],
+  maxHeightIdx: number
+) {
+  const diskStack: Disk[] = []
+  let i: PrevDiskIndex = maxHeightIdx
+
+  while (i !== null) {
+    diskStack.push(disks[i])
+    i = indexes[i]
+  }
+
+  return diskStack.reverse()
+}
