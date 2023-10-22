0f1a605197325184afc68b05aa5f1cd3586d604b
raf0nx <rafalpawlowski2011@gmail.com>
2023-10-22 18:09:23 +0200

    Add more optimal solution for the `Numbers in Pi` algorithm

--- NumbersInPi.ts
+++ NumbersInPi.ts
@@ -1,6 +1,7 @@
+type NumbersTable = Record<string, boolean>
+
 // Solution 1, O(n^2 * m) time complexity, O(n + m) space complexity
 // where n is the length of the `pi` string and m is the length of the `numbers` array
-type NumbersTable = Record<string, boolean>
 type ObjWithMinSpaces = { min: number | null }
 
 export function numbersInPi(pi: string, numbers: string[]) {
@@ -10,12 +11,12 @@ export function numbersInPi(pi: string, numbers: string[]) {
   )
   const objWithMinSpaces: ObjWithMinSpaces = { min: null }
 
-  helper(pi, numbersTable, 0, 0, objWithMinSpaces)
+  getMinSpaces(pi, numbersTable, 0, 0, objWithMinSpaces)
 
   return objWithMinSpaces.min ?? -1
 }
 
-function helper(
+function getMinSpaces(
   pi: string,
   numbersTable: NumbersTable,
   minNumOfSpaces: number,
@@ -33,7 +34,46 @@ function helper(
     const pfx = pi.substring(start, i + 1)
 
     if (pfx in numbersTable) {
-      helper(pi, numbersTable, minNumOfSpaces + 1, i + 1, objWithMinSpaces)
+      getMinSpaces(pi, numbersTable, minNumOfSpaces + 1, i + 1, objWithMinSpaces)
     }
   }
+}
+
+// Solution 2, O(n^3 + m) time complexity, O(n + m) space complexity
+// where n is the length of the `pi` string and m is the length of the `numbers` array
+type Cache = Record<string, number>
+
+export function numbersInPi2(pi: string, numbers: string[]) {
+  const numbersTable: NumbersTable = numbers.reduce(
+    (a, b) => ({ ...a, [b]: true }),
+    {}
+  )
+  const cache: Cache = {}
+
+  const minSpaces = getMinSpaces2(pi, numbersTable, 0, cache)
+
+  return minSpaces === Infinity ? -1 : minSpaces
+}
+
+function getMinSpaces2(
+  pi: string,
+  numbersTable: NumbersTable,
+  idx: number,
+  cache: Cache
+) {
+  if (cache[idx]) return cache[idx]
+  if (idx >= pi.length) return -1
+
+  for (let i = idx; i < pi.length; i++) {
+    const pfx = pi.substring(idx, i + 1)
+
+    if (pfx in numbersTable) {
+      cache[idx] = Math.min(
+        getMinSpaces2(pi, numbersTable, i + 1, cache) + 1,
+        cache[idx] || Infinity
+      )
+    }
+  }
+
+  return cache[idx] ?? Infinity
 }
