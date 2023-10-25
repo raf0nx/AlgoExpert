b7046d9ad1e3dcfcf7451bb504f3739f0405b4a2
raf0nx <rafalpawlowski2011@gmail.com>
2023-10-25 02:01:16 +0200

    Add more optimal solution to the `Maximum Sum Submatrix` algorithm

--- MaximumSumSubmatrix.ts
+++ MaximumSumSubmatrix.ts
@@ -44,3 +44,38 @@ function calculateSubmatrixSum(matrix: number[][], boundaries: Boundaries) {
 
   return sum
 }
+
+// Solution 2, O(w * h), O(w * h) space complexity
+// where w is the width of the matrix and h is the height of the matrix
+export function maximumSumSubmatrix2(matrix: number[][], size: number) {
+  const sums: number[][] = Array.from({ length: matrix.length }, () =>
+    new Array(matrix[0].length).fill(0)
+  )
+
+  let maxSum = -Infinity
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (i === 0) {
+        sums[i][j] = matrix[i][j] + (sums[i][j - 1] ?? 0)
+      } else if (j === 0) {
+        sums[i][j] = matrix[i][j] + sums[i - 1][j]
+      } else {
+        sums[i][j] =
+          matrix[i][j] + sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1]
+      }
+
+      if (i < size - 1 || j < size - 1) continue
+
+      maxSum = Math.max(
+        maxSum,
+        sums[i][j] -
+          (sums[i - size]?.[j] ?? 0) -
+          (sums[i][j - size] ?? 0) +
+          (sums[i - size]?.[j - size] ?? 0)
+      )
+    }
+  }
+
+  return maxSum
+}
