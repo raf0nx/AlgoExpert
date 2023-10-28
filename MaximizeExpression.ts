875cba5866c1b015fa3a890878f6909f7a703345
raf0nx <rafalpawlowski2011@gmail.com>
2023-10-28 15:33:46 +0200

    Add optimal solution to the `Maximize Expression` algorithm

--- MaximizeExpression.ts
+++ MaximizeExpression.ts
@@ -15,3 +15,40 @@ export function maximizeExpression(array: number[]) {
 
   return maxSum === -Infinity ? 0 : maxSum
 }
+
+// Solution 2, O(n) time complexity, O(n) space complexity,
+// where n is the length of the input array
+export function maximizeExpression2(array: number[]) {
+  if (array.length < 4) return 0
+
+  const maxA: number[] = new Array(array.length).fill(-Infinity)
+  const maxAMinusB: number[] = new Array(array.length).fill(-Infinity)
+  const maxAminusBPlusC: number[] = new Array(array.length).fill(-Infinity)
+  const maxAMinusBPlusCMinusD: number[] = new Array(array.length).fill(
+    -Infinity
+  )
+
+  for (let i = 0; i < array.length; i++) {
+    maxA[i] = Math.max(maxA[i - 1] || -Infinity, array[i])
+  }
+
+  for (let i = 1; i < array.length; i++) {
+    maxAMinusB[i] = Math.max(maxAMinusB[i - 1], maxA[i - 1] - array[i])
+  }
+
+  for (let i = 2; i < array.length; i++) {
+    maxAminusBPlusC[i] = Math.max(
+      maxAminusBPlusC[i - 1],
+      maxAMinusB[i - 1] + array[i]
+    )
+  }
+
+  for (let i = 3; i < array.length; i++) {
+    maxAMinusBPlusCMinusD[i] = Math.max(
+      maxAMinusBPlusCMinusD[i - 1],
+      maxAminusBPlusC[i - 1] - array[i]
+    )
+  }
+
+  return maxAMinusBPlusCMinusD[maxAMinusBPlusCMinusD.length - 1]
+}
