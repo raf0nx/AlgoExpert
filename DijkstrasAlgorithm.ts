06f478064dd3d64127e441d5753dff3a5f5c4664
raf0nx <rafalpawlowski2011@gmail.com>
2023-11-02 17:08:40 +0100

    Add more optimal solution to the `Dijkstra's Algorithm` algorithm

--- DijkstrasAlgorithm.ts
+++ DijkstrasAlgorithm.ts
@@ -50,3 +50,120 @@ function findShortestDistanceVertex(
 
   return minDistanceNodeIdx
 }
+
+// Solution 2, O((v + e) * log(v)) time complexity, O(v) space complexity,
+// where v is the number of vertices and e is the number of edges in the input graph
+export function dijkstrasAlgorithm2(start: number, edges: number[][][]) {
+  const shortestPaths: number[] = new Array(edges.length).fill(Infinity)
+  const initialDistances: [number, number][] = Array.from(
+    { length: edges.length },
+    (_, i) => [i, Infinity]
+  )
+  const minDistancesHeap = new MinHeap(initialDistances)
+
+  shortestPaths[start] = 0
+  minDistancesHeap.update(start, 0)
+
+  while (!minDistancesHeap.isEmpty()) {
+    const [vertex, currentMinDistance] = minDistancesHeap.remove()!
+
+    if (currentMinDistance === Infinity) break
+
+    for (const [destination, distanceToDestination] of edges[vertex]) {
+      const newPath = distanceToDestination + currentMinDistance
+      const currentPath = shortestPaths[destination]
+
+      if (newPath < currentPath) {
+        shortestPaths[destination] = newPath
+        minDistancesHeap.update(destination, newPath)
+      }
+    }
+  }
+
+  return shortestPaths.map(path => (path === Infinity ? -1 : path))
+}
+
+class MinHeap {
+  vertexMap: { [vertex: number]: number }
+  heap: [number, number][]
+
+  constructor(array: [number, number][]) {
+    this.vertexMap = array.reduce((obj, _, i) => {
+      obj[i] = i
+      return obj
+    }, {} as { [vertex: number]: number })
+    this.heap = this.buildHeap(array)
+  }
+
+  isEmpty() {
+    return this.heap.length === 0
+  }
+
+  buildHeap(array: [number, number][]) {
+    const firstParentIdx = Math.floor((array.length - 2) / 2)
+
+    for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+      this.siftDown(currentIdx, array.length - 1, array)
+    }
+
+    return array
+  }
+
+  siftDown(currentIdx: number, endIdx: number, heap: [number, number][]) {
+    let childOneIdx = currentIdx * 2 + 1
+
+    while (childOneIdx <= endIdx) {
+      const childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1
+      let idxToSwap
+
+      if (childTwoIdx !== -1 && heap[childTwoIdx][1] < heap[childOneIdx][1]) {
+        idxToSwap = childTwoIdx
+      } else {
+        idxToSwap = childOneIdx
+      }
+
+      if (heap[idxToSwap][1] < heap[currentIdx][1]) {
+        this.swap(currentIdx, idxToSwap, heap)
+        currentIdx = idxToSwap
+        childOneIdx = currentIdx * 2 + 1
+      } else {
+        return
+      }
+    }
+  }
+
+  siftUp(currentIdx: number, heap: [number, number][]) {
+    let parentIdx = Math.floor((currentIdx - 1) / 2)
+
+    while (currentIdx > 0 && heap[currentIdx][1] < heap[parentIdx][1]) {
+      this.swap(currentIdx, parentIdx, heap)
+      currentIdx = parentIdx
+      parentIdx = Math.floor((currentIdx - 1) / 2)
+    }
+  }
+
+  remove() {
+    if (this.isEmpty()) return
+
+    this.swap(0, this.heap.length - 1, this.heap)
+    const [vertex, distance] = this.heap.pop()!
+    delete this.vertexMap[vertex]
+    this.siftDown(0, this.heap.length - 1, this.heap)
+
+    return [vertex, distance]
+  }
+
+  swap(i: number, j: number, heap: [number, number][]) {
+    this.vertexMap[heap[i][0]] = j
+    this.vertexMap[heap[j][0]] = i
+
+    const temp = heap[j]
+    heap[j] = heap[i]
+    heap[i] = temp
+  }
+
+  update(vertex: number, value: number) {
+    this.heap[this.vertexMap[vertex]] = [vertex, value]
+    this.siftUp(this.vertexMap[vertex], this.heap)
+  }
+}
