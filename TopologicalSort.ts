09f60e8f9ed695f2aea8d2050ba3d3b39b189cc0
raf0nx <rafalpawlowski2011@gmail.com>
2023-11-04 02:57:01 +0100

    Add more optimal solution to the `Topological Sort` algorithm

--- TopologicalSort.ts
+++ TopologicalSort.ts
@@ -29,3 +29,48 @@ export function topologicalSort(jobs: number[], deps: Dependency[]) {
 
   return jobs
 }
+
+// Solution 2, O(j * d) time complexity, O(j) space complexity,
+// where j is the number of jobs and d is the number of dependencies
+type JobsIdxsMap = Record<number, number>
+
+export function topologicalSort2(jobs: number[], deps: Dependency[]) {
+  const jobsIdxsMap: JobsIdxsMap = jobs.reduce(
+    (map, job, i) => ({ ...map, [job]: i }),
+    {}
+  )
+  let hasSorted = true
+  let sortsLeft = jobs.length
+
+  while (hasSorted) {
+    if (!sortsLeft) return []
+
+    hasSorted = false
+
+    for (const [firstJob, secondJob] of deps) {
+      const firstJobIdx = jobsIdxsMap[firstJob]
+      const secondJobIdx = jobsIdxsMap[secondJob]
+
+      if (firstJobIdx > secondJobIdx) {
+        const temp = jobsIdxsMap[firstJob]
+        jobsIdxsMap[firstJob] = jobsIdxsMap[secondJob]
+        jobsIdxsMap[secondJob] = temp
+        hasSorted = true
+      }
+    }
+
+    sortsLeft -= 1
+  }
+
+  return createSortedJobsArray(jobsIdxsMap)
+}
+
+function createSortedJobsArray(jobsIdxsMap: JobsIdxsMap) {
+  const sortedJobs = new Array(Object.keys(jobsIdxsMap).length)
+
+  for (const [value, idx] of Object.entries(jobsIdxsMap)) {
+    sortedJobs[idx] = +value
+  }
+
+  return sortedJobs
+}
