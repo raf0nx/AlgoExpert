ad2b226f5f490a5d49843eb5205f72d82549b312
raf0nx <rafalpawlowski2011@gmail.com>
2023-11-05 03:29:09 +0100

    Add the most optimal solution for the `Topological Sort` algorithm

--- TopologicalSort.ts
+++ TopologicalSort.ts
@@ -74,3 +74,99 @@ function createSortedJobsArray(jobsIdxsMap: JobsIdxsMap) {
 
   return sortedJobs
 }
+
+// Solution 3, O(j + d) time complexity, O(j + d) space complexity,
+// where j is the number of jobs and d is the number of dependencies
+export function topologicalSort3(jobs: number[], deps: Dependency[]) {
+  const jobGraph = createJobGraph(jobs, deps)
+  return getOrderedJobs(jobGraph)
+}
+
+function createJobGraph(jobs: number[], deps: Dependency[]) {
+  const jobGraph = new JobGraph(jobs)
+
+  for (const [prereq, job] of deps) {
+    jobGraph.addPrereq(job, prereq)
+  }
+
+  return jobGraph
+}
+
+function getOrderedJobs(jobGraph: JobGraph) {
+  const orderedJobs: number[] = []
+  const { nodes } = jobGraph
+
+  while (nodes.length) {
+    const node = nodes.pop()!
+    const containsCycle = dfs(node, orderedJobs)
+
+    if (containsCycle) return []
+  }
+
+  return orderedJobs
+}
+
+function dfs(currentNode: JobNode, orderedJobs: number[]): boolean | void {
+  if (currentNode.visited) return false
+  if (currentNode.visiting) return true
+
+  currentNode.visiting = true
+
+  for (const prereqNode of currentNode.prereqs) {
+    const containsCycle = dfs(prereqNode, orderedJobs)
+
+    if (containsCycle) return true
+  }
+
+  orderedJobs.push(currentNode.job)
+  currentNode.visited = true
+  currentNode.visiting = false
+
+  return false
+}
+
+class JobGraph {
+  nodes: JobNode[]
+  graph: Record<number, JobNode>
+
+  constructor(jobs: number[]) {
+    this.nodes = []
+    this.graph = {}
+
+    for (const job of jobs) {
+      this.addNode(job)
+    }
+  }
+
+  addNode(job: number) {
+    this.graph[job] = new JobNode(job)
+    this.nodes.push(this.graph[job])
+  }
+
+  addPrereq(job: number, prereq: number) {
+    const jobNode = this.getNode(job)
+    const prereqNode = this.getNode(prereq)
+
+    jobNode.prereqs.push(prereqNode)
+  }
+
+  getNode(job: number) {
+    if (!(job in this.graph)) this.addNode(job)
+
+    return this.graph[job]
+  }
+}
+
+class JobNode {
+  job: number
+  prereqs: JobNode[]
+  visited: boolean
+  visiting: boolean
+
+  constructor(job: number) {
+    this.job = job
+    this.prereqs = []
+    this.visited = false
+    this.visiting = false
+  }
+}
