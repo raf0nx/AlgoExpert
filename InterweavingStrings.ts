82f9a55f69d77ed1af2bb74c1ed66346fe0b95fb
raf0nx <rafalpawlowski2011@gmail.com>
2023-12-10 17:05:46 +0100

    Optimize the `Interweaving Strings` algortihm solution using caching

--- InterweavingStrings.ts
+++ InterweavingStrings.ts
@@ -22,3 +22,51 @@ export function interweavingStrings(
 
   return false
 }
+
+// Solution 2, O(n * m) time complexity, O(n * m) space complexity,
+// where n is the length of the first string and m is the length of the second string
+export function interweavingStrings2(
+  one: string,
+  two: string,
+  three: string,
+  i = 0,
+  j = 0,
+  k = 0,
+  cache: Record<string, boolean> = {}
+): boolean {
+  if (one.length + two.length !== three.length) return false
+
+  if (cache[`${i}${j}${k}`]) return cache[`${i}${j}${k}`]
+
+  if (k === three.length) return true
+
+  if (i < one.length && one[i] === three[k]) {
+    cache[`${i}${j}${k}`] = interweavingStrings2(
+      one,
+      two,
+      three,
+      i + 1,
+      j,
+      k + 1,
+      cache
+    )
+    if (cache[`${i}${j}${k}`]) return true
+  }
+
+  if (j < two.length && two[j] === three[k]) {
+    cache[`${i}${j}${k}`] = interweavingStrings2(
+      one,
+      two,
+      three,
+      i,
+      j + 1,
+      k + 1,
+      cache
+    )
+    return cache[`${i}${j}${k}`]
+  }
+
+  cache[`${i}${j}${k}`] = false
+
+  return false
+}
