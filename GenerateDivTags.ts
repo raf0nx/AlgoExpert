10f3854d3eaf7a7fd970b2ca348de896ed0b0a3f
raf0nx <rafalpawlowski2011@gmail.com>
2023-12-16 21:45:41 +0100

    Add more optimal solution to the `Generate Div Tags` algorithm

--- GenerateDivTags.ts
+++ GenerateDivTags.ts
@@ -31,3 +31,41 @@ function divTagGenerator(
 
   return divTagGenerator(numberOfTags - 1, divTags)
 }
+
+// Solution 2, O((2 * n)!/((n! * ((n + 1)!)))) time complexity | O((2 * n)!/((n! * ((n + 1)!)))) space complexity,
+// where n is the input number
+export function generateDivTags2(numberOfTags: number) {
+  return divTagGenerator2('', numberOfTags, numberOfTags)
+}
+
+function divTagGenerator2(
+  partialString: string,
+  openingTagsLeft: number,
+  closingTagsLeft: number
+) {
+  if (closingTagsLeft === 0) return [partialString]
+
+  const divTags: string[] = []
+
+  if (openingTagsLeft > 0) {
+    divTags.push(
+      ...divTagGenerator2(
+        partialString.concat('<div>'),
+        openingTagsLeft - 1,
+        closingTagsLeft
+      )
+    )
+  }
+
+  if (closingTagsLeft > openingTagsLeft) {
+    divTags.push(
+      ...divTagGenerator2(
+        partialString.concat('</div>'),
+        openingTagsLeft,
+        closingTagsLeft - 1
+      )
+    )
+  }
+
+  return divTags
+}
