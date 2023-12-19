f7076d4009e31cf0ebefa10de5f0646dd708db8d
raf0nx <rafalpawlowski2011@gmail.com>
2023-12-19 20:34:10 +0100

    Add another solution to the `Ambiguous Measurements` algorithm

--- AmbiguousMeasurements.ts
+++ AmbiguousMeasurements.ts
@@ -40,3 +40,43 @@ function canMeasureInRange(
 
   return false
 }
+
+// Solution 2, O(low * high * n) time complexity, O(low * high) space complexity,
+// where n is the number of measuring cups
+export function ambiguousMeasurements2(
+  measuringCups: number[][],
+  low: number,
+  high: number
+) {
+  return canMeasureInRange2(measuringCups, low, high, {})
+}
+
+function canMeasureInRange2(
+  measuringCups: number[][],
+  low: number,
+  high: number,
+  cache: Record<string, boolean>
+): boolean {
+  if (`${low}-${high}` in cache) return cache[`${low}-${high}`]
+
+  if (low <= 0 && high <= 0) return false
+
+  let canMeasure = false
+
+  for (const [cupLow, cupHigh] of measuringCups) {
+    if (low <= cupLow && cupHigh <= high) {
+      canMeasure = true
+      break
+    }
+
+    const newLow = Math.max(0, low - cupLow)
+    const newHigh = Math.max(0, high - cupHigh)
+    canMeasure = canMeasureInRange2(measuringCups, newLow, newHigh, cache)
+
+    if (canMeasure) break
+  }
+
+  cache[`${low}-${high}`] = canMeasure
+
+  return canMeasure
+}
