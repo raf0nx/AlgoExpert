255601aa583cab293cb1979ac8b0c2ee3567c83b
raf0nx <rafalpawlowski2011@gmail.com>
2023-12-26 16:48:35 +0100

    Add more optimal, iterative solution to the `Shifted Binary Search` algorithm

--- ShiftedBinarySearch.ts
+++ ShiftedBinarySearch.ts
@@ -26,3 +26,25 @@ export function shiftedBinarySearch(
     }
   }
 }
+
+// Solution 2, O(log(n)) time complexity, O(1) space complexity,
+// where n is the length of the input array
+export function shiftedBinarySearch2(array: number[], target: number) {
+  let left = 0
+  let right = array.length - 1
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+
+    if (array[mid] === target) return mid
+    else if (array[left] <= array[mid]) {
+      if (array[left] <= target && target < array[mid]) right = mid - 1
+      else left = mid + 1
+    } else {
+      if (array[mid] < target && target <= array[right]) left = mid + 1
+      else right = mid - 1
+    }
+  }
+
+  return -1
+}
