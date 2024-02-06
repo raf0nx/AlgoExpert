b0940592085e2ec6d012e6e45666e2c28bc3b40d
raf0nx <rafalpawlowski2011@gmail.com>
2024-02-06 08:47:21 +0100

    Improve the readability for the Solution 1 for the `Count Squares` algorithm

--- CountSquares.ts
+++ CountSquares.ts
@@ -1,23 +1,24 @@
 // Solution 1, O(n^2) time complexity, O(n) space complexity,
 // where n is the number of points
 export function countSquares(points: number[][]) {
-  const pointsSet = new Set(points.map(([x, y]) => `${x},${y}`))
+  const pointsSet = new Set(points.map(point => point.join(',')))
   let numOfSquares = 0
 
   for (let i = 0; i < points.length; i++) {
     for (let j = i + 1; j < points.length; j++) {
-      const [xX, xY] = points[i]
-      const [yX, yY] = points[j]
-      const midX = (xX + yX) / 2
-      const midY = (xY + yY) / 2
-      const xDistance = xX - midX
-      const yDistance = xY - midY
-      const zX = midX + yDistance
-      const zY = midY - xDistance
-      const wX = midX - yDistance
-      const wY = midY + xDistance
+      const [pointAX, pointAY] = points[i]
+      const [pointBX, pointBY] = points[j]
 
-      if (pointsSet.has(`${zX},${zY}`) && pointsSet.has(`${wX},${wY}`))
+      const midX = (pointAX + pointBX) / 2
+      const midY = (pointAY + pointBY) / 2
+
+      const xDistanceFromMid = pointAX - midX
+      const yDistanceFromMid = pointAY - midY
+
+      const pointC = [midX + yDistanceFromMid, midY - xDistanceFromMid]
+      const pointD = [midX - yDistanceFromMid, midY + xDistanceFromMid]
+
+      if (pointsSet.has(pointC.join(',')) && pointsSet.has(pointD.join(',')))
         numOfSquares++
     }
   }
