fd62db7c50f4efe2c5d04724e9364f6dc9415065
raf0nx <rafalpawlowski2011@gmail.com>
2024-02-08 09:07:49 +0100

    Add solution to the `Largest Island` algorithm

---
+++ LargestIsland.ts
@@ -0,0 +1,50 @@
+// Solution 1, O((w * h)^2) time complexity, O(w * h) space complexity,
+// where w is the width of the matrix and h is the height of the matrix
+export function largestIsland(matrix: number[][]) {
+  let largestLand = 0
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) continue
+
+      matrix[i][j] = 0
+      largestLand = Math.max(largestLand, findTheGreatestLand(matrix, i, j, {}))
+      matrix[i][j] = 1
+    }
+  }
+
+  return largestLand
+}
+
+function findTheGreatestLand(
+  matrix: number[][],
+  i: number,
+  j: number,
+  visited: Record<string, boolean>
+) {
+  if (visited[`${i},${j}`] || isOutOfBound(matrix, i, j) || matrix[i][j] === 1)
+    return 0
+
+  visited[`${i},${j}`] = true
+
+  let landSize = 1
+
+  for (let [nextRow, nextCol] of getDirections(i, j)) {
+    landSize += findTheGreatestLand(matrix, nextRow, nextCol, visited)
+  }
+
+  return landSize
+}
+
+function isOutOfBound(matrix: number[][], i: number, j: number) {
+  return i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length
+}
+
+function getDirections(i: number, j: number) {
+  return [
+    [i - 1, j],
+    [i, j + 1],
+    [i + 1, j],
+    [i, j - 1],
+  ]
+}
