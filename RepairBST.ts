4e70012a72481d8b946b7b9c480a0bda1c251966
raf0nx <rafalpawlowski2011@gmail.com>
2024-02-15 10:10:03 +0100

    Add iterative solution to the `Repair BST` algorithm

--- RepairBST.ts
+++ RepairBST.ts
@@ -54,3 +54,40 @@ function swapNodes(treeInfo: TreeInfo) {
   treeInfo.nodeOne.value = treeInfo.nodeTwo.value
   treeInfo.nodeTwo.value = temp
 }
+
+// Solution 2, O(n) time complexity, O(h) space complexity,
+// where n is the number of nodes in the tree and h is the height of the tree
+export function repairBst2(tree: BST) {
+  const stack: BST[] = []
+  let nodeOne: BST = { value: Infinity, left: null, right: null }
+  let nodeTwo: BST = { value: Infinity, left: null, right: null }
+  let prevNode: BST = { value: -Infinity, left: null, right: null }
+  let currentNode: BST | null = tree
+
+  while (currentNode || stack.length) {
+    while (currentNode) {
+      stack.push(currentNode)
+      currentNode = currentNode.left
+    }
+
+    currentNode = stack.pop()!
+
+    if (currentNode.value < prevNode.value) {
+      if (nodeOne.value === Infinity) nodeOne = prevNode
+      nodeTwo = currentNode
+    }
+
+    prevNode = currentNode
+    currentNode = currentNode.right
+  }
+
+  swapNodes2(nodeOne, nodeTwo)
+
+  return tree
+}
+
+function swapNodes2(nodeOne: BST, nodeTwo: BST) {
+  const temp = nodeOne.value
+  nodeOne.value = nodeTwo.value
+  nodeTwo.value = temp
+}
