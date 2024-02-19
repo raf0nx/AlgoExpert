0d88529ca3f5fb654dd27383ab10a11b1c6baa67
raf0nx <rafalpawlowski2011@gmail.com>
2024-02-19 22:29:02 +0100

    Add more optimal solution to the `Longest Most Frequent Prefix` algorithm

--- LongestMostFrequentPrefix.ts
+++ LongestMostFrequentPrefix.ts
@@ -17,3 +17,39 @@ export function longestMostFrequentPrefix(strings: string[]) {
 
   return mostCommonPrefixes.reduce((a, b) => (a.length > b.length ? a : b), '')
 }
+
+// Solution 2, O(n * m) time complexity, O(n * m) space complexity,
+// where n is the number of strings and m is the length of the longest string
+export function longestMostFrequentPrefix2(strings: string[]) {
+  const trie: Record<string, any> = {}
+
+  let maxPrefixCount = 0
+  let maxPrefixLength = 0
+  let maxPrefixFullString = ''
+
+  for (const string of strings) {
+    let currentNode = trie
+
+    for (const char of string) {
+      if (!(char in currentNode)) currentNode[char] = {}
+
+      currentNode[char].count = (currentNode[char].count || 0) + 1
+      currentNode[char].length = (currentNode.length || 0) + 1
+      currentNode = currentNode[char]
+
+      if (currentNode.count > maxPrefixCount) {
+        maxPrefixCount = currentNode.count
+        maxPrefixLength = currentNode.length
+        maxPrefixFullString = string
+      } else if (
+        currentNode.count === maxPrefixCount &&
+        currentNode.length > maxPrefixLength
+      ) {
+        maxPrefixLength = currentNode.length
+        maxPrefixFullString = string
+      }
+    }
+  }
+
+  return maxPrefixFullString.slice(0, maxPrefixLength)
+}
